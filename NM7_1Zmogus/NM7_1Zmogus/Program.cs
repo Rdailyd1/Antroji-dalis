325fad5a27d5348c25ecc6f4e13d8dc5902a0769
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-21 21:27:22 +0300

    Žmogus, vardas, pavardė

---
+++ NM7_1Zmogus/NM7_1Zmogus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NM7_1Zmogus
+{
+    class Program
+    {
+        public struct Zmogus
+        {
+            public string vardas;
+            public string pavarde;
+        }
+        static void Main(string[] args)
+        {
+            Zmogus vyras = new Zmogus();
+            vyras.vardas = "Antanas";
+            vyras.pavarde = "Antanauskas";
+            Zmogus moteris = new Zmogus();
+            moteris.vardas = "Rita";
+            moteris.pavarde = "Ritenyte";
+            Console.WriteLine("Zmogus Nr.1: {0} {1}", vyras.vardas, vyras.pavarde);
+            Console.WriteLine("Zmogus Nr.2: {0} {1}", moteris.vardas, moteris.pavarde);
+            Console.ReadLine();
+        }
+    }
+}
+
