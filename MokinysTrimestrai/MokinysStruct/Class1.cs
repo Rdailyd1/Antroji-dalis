f9d2755ed0f9ab30a6672580cae6f2852e256f71
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-23 19:45:32 +0300

    Trimestrų skaičiavimas

---
+++ MokinysTrimestrai/MokinysStruct/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MokinysStruct
+{
+    public struct Mokinys
+    {
+        public List<int> trimestras1;
+        public List<int> trimestras2;
+        public List<int> trimestras3;
+        public List<int> bendras;
+        
+        public int PirmojoTrimestroVidurkis()
+        {
+            int sum=0;
+            int vid=0;
+
+            foreach (int elementas in trimestras1)
+            {
+                sum += elementas;
+            }
+            vid = sum / trimestras1.Count;
+                return vid;
+        }
+        public int AntrojoTrimestroVidurkis()
+        {
+            int sum = 0;
+            int vid = 0;
+
+            foreach (int elementas in trimestras2)
+            {
+                sum += elementas;
+            }
+            vid = sum / trimestras2.Count;
+            return vid;
+        }
+        public int TrecioTrimestroVidurkis()
+        {
+            int sum = 0;
+            int vid = 0;
+
+            foreach (int elementas in trimestras3)
+            {
+                sum += elementas;
+            }
+            vid = sum / trimestras3.Count;
+            return vid;
+        }
+        public int MetinisPazymys()
+        {
+            int sum = 0;
+            var bendras = new List<int>();
+            bendras.AddRange(trimestras1);
+            bendras.AddRange(trimestras2);
+            bendras.AddRange(trimestras3);
+            foreach (int elementas in bendras)
+            {
+                sum += elementas;
+            }
+            return sum;
+        }
+    }
+}
---
+++ MokinysTrimestrai/MokinysTrimestrai/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MokinysStruct;
+
+namespace MokinysTrimestrai
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            
+            Mokinys mokinys = new Mokinys();
+            mokinys.trimestras1 = new List<int>();
+            mokinys.trimestras2 = new List<int>();
+            mokinys.trimestras3 = new List<int>();
+
+            mokinys.trimestras1.Add(5);
+            mokinys.trimestras1.Add(7);
+            mokinys.trimestras1.Add(6);
+            mokinys.trimestras2.Add(4);
+            mokinys.trimestras2.Add(7);
+            mokinys.trimestras2.Add(9);
+            mokinys.trimestras3.Add(4);
+            mokinys.trimestras3.Add(5);
+            mokinys.trimestras3.Add(5);
+            Console.WriteLine("Pirmojo trimestro vidurkis {0}", mokinys.PirmojoTrimestroVidurkis());
+            Console.WriteLine("Pirmojo trimestro vidurkis {0}", mokinys.AntrojoTrimestroVidurkis());
+            Console.WriteLine("Pirmojo trimestro vidurkis {0}", mokinys.TrecioTrimestroVidurkis());
+            Console.WriteLine("Metinis pazymys {0}", (mokinys.PirmojoTrimestroVidurkis() + mokinys.AntrojoTrimestroVidurkis() + mokinys.TrecioTrimestroVidurkis())/3);
+            Console.ReadLine();
+        }
+    }
+}
