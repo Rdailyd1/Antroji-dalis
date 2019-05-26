80f15e3abe1e575c2d3071951be121e3874fb35d
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-26 19:39:41 +0300

    Sąskaitos

---
+++ Saskaitos/Saskaita/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Saskaita
+{
+    public struct saskaita
+    {
+        private string _gavejas;
+        private string _siuntejas;
+        private int _moketiViso;
+       
+        public string Gavejas
+        {
+            get
+            {
+                return _gavejas;
+            }
+            private set
+            {
+                _gavejas = value;
+            }
+        }
+
+        public string Siuntejas
+        {
+            get
+            {
+                return _siuntejas;
+            }
+            private set
+            {
+                _siuntejas = value;
+            }
+        }
+
+        public int MoketiViso
+        {
+            get
+            {
+                return _moketiViso;
+            }
+            private set
+            {
+                _moketiViso = value;
+            }
+        }
+
+        public string SaskaitosNumeris
+        {
+            get
+            {
+                return string.Format("NR {0}_{1}_{2}", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            }
+        }
+
+        public saskaita(string gavejas, string siuntejas, int moketiviso)
+        {
+            _siuntejas = siuntejas;
+            _gavejas = gavejas;
+            _moketiViso = moketiviso;
+        }
+
+    }
+}
---
+++ Saskaitos/Saskaitos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Saskaita;
+
+namespace Saskaitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            saskaita sask = new saskaita("Imone","vst",250);
+            
+            Console.WriteLine(sask.Gavejas);
+            Console.WriteLine(sask.Siuntejas);
+            Console.WriteLine(sask.SaskaitosNumeris);
+            Console.WriteLine(sask.MoketiViso);
+            Console.ReadLine();
+        }
+    }
+}
