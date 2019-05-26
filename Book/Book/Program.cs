91702923174ed6d8fdbde6da2b1bb1e70f9a8d2f
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-26 19:40:54 +0300

    Book

---
+++ Book/Book/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStruct;
+
+namespace BookProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book knyga = new Book("Vardas", "123");
+            //knyga.LocalId = string.Format("{0}_{1}", knyga.Name, knyga.Id);
+
+            Console.WriteLine(knyga.Name);
+            Console.WriteLine(knyga.Id);
+            //Console.WriteLine(knyga.GetId());
+            Console.WriteLine(knyga.LocalId);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Book/BookStruct/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BookStruct
+{
+    public struct Book
+    {
+        public string _name;
+        private string _id;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                _id = value;
+            }
+        }
+        public Book(string name, string id)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        public string LocalId
+        {
+            get
+            {
+                return string.Format("{0}_{1}", Name, _id);
+            }
+        }
+    }
+}
