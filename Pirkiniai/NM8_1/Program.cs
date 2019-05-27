99f0f74487488b51d94ccd8632a666c88776e06b
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-27 14:30:58 +0300

    NM8_1 pirkiniai

---
+++ Pirkiniai/NM8_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pirkinys;
+namespace NM8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pirkinys pirk = new pirkinys();
+            pirk._PrekėsPavadinimas = "Bulves";
+            pirk._Kiekis = 5;
+            pirk._KainaVieneto = 1.9m;
+            Console.WriteLine("Prekė: {0}, {1} kg, {2} eur", pirk._PrekėsPavadinimas, pirk._Kiekis, pirk._KainaVieneto);
+            Console.WriteLine("Kaina viso: {0} eur", pirk.KainaViso);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pirkiniai/Pirkinys/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Pirkinys
+{
+    public struct pirkinys
+    {
+        public string _PrekėsPavadinimas;
+        public int _Kiekis;
+        public decimal _KainaVieneto;
+        public decimal _KainaViso;
+
+        public string PrekesPaadinimas
+        {
+            get
+            {
+                return _PrekėsPavadinimas;
+            }
+            private set
+            {
+                _PrekėsPavadinimas = value;
+            }
+        }
+        public int Kiekis
+        {
+            get
+            {
+                return _Kiekis;
+            }
+            private set
+            {
+                _Kiekis = value;
+            }
+        }
+        public decimal KainaVieneto
+        {
+            get
+            {
+                return _KainaVieneto;
+            }
+            private set
+            {
+                _KainaVieneto = value;
+            }
+        }
+        public decimal KainaViso
+        {
+            get
+            {
+                _KainaViso = _KainaVieneto*_Kiekis;
+                return _KainaViso;
+            }
+        } 
+    }
+}
