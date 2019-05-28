bc14d55ab4349990e137d327eed12787b3da958b
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-28 20:44:43 +0300

    Pirkiniai Unit testai

---
+++ NM8_4/UnitTestProject_NM8/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject_NM8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject_NM8")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9251d006-7f37-45af-a175-d1f5495704da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NM8_4/UnitTestProject_NM8/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Pirkinys;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_NM8
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void kainaTuriButi2302()
+        {
+            // Asign
+            pirkinys vardas = new pirkinys("stalas", 10, 230.20m);
+            //Act
+            decimal kainaViso = vardas.KainaViso;
+            //Assert
+           
+            Assert.AreEqual(2302, kainaViso);
+        }
+
+        [TestMethod]
+        public void kainaTuriButi0KaiKiekis0()
+        {
+            // Asign
+            pirkinys vardas = new pirkinys("stalas", 0, 230.20m);
+            //Act
+            decimal kainaViso = vardas.KainaViso;
+            //Assert
+
+            Assert.AreEqual(0, kainaViso);
+        }
+        [TestMethod]
+        public void kainaTuriButi0KaiVienetoKaina0()
+        {
+            // Asign
+            pirkinys vardas = new pirkinys("spinta", 5, 0);
+            //Act
+            decimal kainaViso = vardas.KainaViso;
+            //Assert
+
+            Assert.AreEqual(0, kainaViso);
+        }
+    }
+}
