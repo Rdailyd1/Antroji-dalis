2d0c9004f9ff3d6bf51d1c29b6facde8f10a7557
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-31 21:47:04 +0300

    Klasės Address pridėjimas

---
+++ CRM/CRM.BL/Address.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.BL
+{
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        public Address(int addressId)
+        {
+            AddressId = addressId;
+        }
+        public int AddressId { get; set;}
+        public int AddressType { get; set;}
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(PostalCode))
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+    
+}
