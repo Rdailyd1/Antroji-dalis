67818582bb9cbfbc38bb24cf3eb0cc90f6a1c5e5
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-31 21:08:30 +0300

    CRM.BL

---
+++ CRM/CRM.BL.TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CRM.BL.TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CRM.BL.TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b13abdb-f2ee-4978-90a3-b4cd57a170d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CRM/CRM.BL.TEST/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CRM.BL.TEST
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins, Bilbo";
+           
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            {
+                customer.FirstName = "Bilbo";
+            };
+
+            string expected = "Bilbo";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            {
+                customer.LastName = "Baggins";
+            };
+
+            string expected = "Baggins";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateLastNameEmail()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            {
+                customer.LastName = "Baggins";
+                customer.EmailAddress = "asdd@dfg.lt";
+            };
+
+            bool expected = true;
+
+            //--Act
+            bool actual = customer.Validate();
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+           
+
+    }
+}
