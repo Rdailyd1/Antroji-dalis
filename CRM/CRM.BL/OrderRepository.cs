67ee3e3211002422018e29b567b304aa8ee1827c
Rolandas Dailyde <rdailyd@gmail.com>
2019-06-03 10:43:44 +0300

    OrderRepository ir ProductRepository klasių pridėjimas

---
+++ CRM/CRM.BL/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.BL
+{
+    class OrderRepository
+    {
+        public List<OrderRepository> Retrieve()
+        {
+            return new List<OrderRepository>();
+        }
+        public bool Save()
+        {
+
+            return true;
+        }
+    }
+}
---
+++ CRM/CRM.BL/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.BL
+{
+    class ProductRepository
+    {
+        public List<ProductRepository> Retrieve()
+        {
+            return new List<ProductRepository>();
+        }
+        public bool Save()
+        {
+
+            return true;
+        }
+    }
+}
