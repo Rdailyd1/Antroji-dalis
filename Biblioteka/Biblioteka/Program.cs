5782ef8ab09e8ca1e51051bf121a8560454443ad
Rolandas Dailyde <rdailyd@gmail.com>
2019-06-10 18:54:41 +0300

    NM11 biblioteka, knygos grąžinimas

---
+++ Biblioteka/Biblioteka/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotekosKnyga;
+namespace Biblioteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var knygos = new BibliotekosKnygaCs();
+            Console.WriteLine("Skaitytojo vardas:");
+            string vardas = Console.ReadLine();
+            int ID = 10;
+            string pavadinimas = "Gera knyga";
+            Console.WriteLine("Paėmimo data:");
+            DateTime paemimoData = Convert.ToDateTime(Console.ReadLine());
+            if(knygos.ArLaikuGrazino(ID, paemimoData))
+            {
+                Console.WriteLine("Knyga grazino laiku");
+            }
+            else
+            {
+                Console.WriteLine("Pavelavo grazinti knyga");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Biblioteka/BibliotekosKnyga/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BibliotekosKnyga
+{
+    public class BibliotekosKnygaCs
+    {
+        public int ID { get; set; }
+        public string pavadinimas { get; private set; }
+        public int dienosGrazinti { get; private set; }
+        public string skaitytojoVardas { get; set; }
+        public DateTime paemimoData { get; set; }
+        public bool ArLaikuGrazino(int dienosgrazinti, DateTime paemimodata)
+        {
+
+            bool arLaiku = true;
+            DateTime grazinimoData = paemimodata.AddDays(dienosgrazinti);
+            if (grazinimoData <= DateTime.Now)
+            {
+                arLaiku = false;
+            }
+                return arLaiku;
+        }
+    }
+}
