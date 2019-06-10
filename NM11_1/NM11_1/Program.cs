207fd5a028fe24c8ebe53b7ec4de72d689ca1595
Rolandas Dailyde <rdailyd@gmail.com>
2019-06-10 15:14:12 +0300

    NM11 4-a užduotis, metodo GetAge pridėjimas

--- NM11_1/NM11_1/Program.cs
+++ NM11_1/NM11_1/Program.cs
@@ -12,7 +12,15 @@ namespace NM11_1
         static void Main(string[] args)
         {
             var VardasPavarde = new VardasPavarde_cs();
-            Console.WriteLine("{0} {1}", VardasPavarde.Vardas("Rolandas"), VardasPavarde.Pavarde("Dailydė"));
+
+            Console.WriteLine("Iveskite varda:");
+            string vardas = Console.ReadLine();
+            Console.WriteLine("Iveskite pavarde:");
+            string pavarde = Console.ReadLine();
+            Console.WriteLine("Iveskite gimimo metus:");
+            DateTime  gimMetai= Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("{0} {1} {2}", VardasPavarde.Vardas(vardas), VardasPavarde.Pavarde(pavarde), VardasPavarde.GimMetai(gimMetai));
+            Console.WriteLine("Jums yra {0} m", VardasPavarde.GetAge());
             Console.ReadLine();
         }
     }
--- NM11_1/VardasPavarde/Class1.cs
+++ NM11_1/VardasPavarde/Class1.cs
@@ -5,6 +5,7 @@ namespace VardasPavarde
     {
         private string _vardas { get; set; }
         private string _pavarde { get; set; }
+        public DateTime _GimMetai { get; private set; } 
         public string Vardas(string vardas)
         {
             _vardas = vardas;
@@ -18,6 +19,18 @@ namespace VardasPavarde
             {
                 return _pavarde;
             }
+        }
+        public DateTime GimMetai(DateTime gimMetai)
+        {
+            _GimMetai = gimMetai;
+            {
+                return _GimMetai;
+            }
+        }
+        public int GetAge()
+        {
+            int age = Convert.ToInt16((DateTime.Now -_GimMetai).TotalDays/ 365);
+            return age;
         }
     }
        
