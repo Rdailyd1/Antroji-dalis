b1d7422cc756110ad0ca7eed35d716adb9ba4e4d
Rolandas Dailyde <rdailyd@gmail.com>
2019-06-10 14:12:20 +0300

    Vardas pavardė

---
+++ NM11_1/NM11_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VardasPavarde;
+
+namespace NM11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var VardasPavarde = new VardasPavarde_cs();
+            VardasPavarde.vardas = "Rolandas";
+            VardasPavarde.pavarde = "Dailydė";
+            Console.WriteLine("{0} {1}", VardasPavarde.vardas, VardasPavarde.pavarde);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NM11_1/VardasPavarde/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace VardasPavarde
+{
+    public class VardasPavarde_cs
+    {
+        public string vardas { get; set; }
+        public string pavarde { get; set; }
+    }
+}
