f392ef9fa8a978f5546989425bd5d0282e8f7d4d
Rolandas Dailyde <rdailyd@gmail.com>
2019-06-21 22:13:37 +0300

    Darbas su failais

---
+++ WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using WorkingWithFilesBL;
+
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Analizuoju komandines eilutes parametrus");
+            var command = args[0];
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                Console.WriteLine($"Pasirinkta viena byla {filePath}");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                Console.WriteLine($"Pasirinktas katalogas {directoryPath} ir bylostipas {fileType}");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                Console.WriteLine("Nezinomas komandines eilutes parametras {0}", command);
+            }
+            Console.WriteLine("Press ENTER to quit");
+            Console.ReadLine();
+        }
+        private static void ProcessSingleFile(string filePath)
+        {
+
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            switch (fileType)
+            {
+                case "TEXT":
+                    string[] textFiles = Directory.GetFiles(directoryPath, "*.txt");
+                    foreach (var textFilePath in textFiles)
+                    {
+                        Console.WriteLine(textFilePath);
+                        //var fileProcessor = new FileProcessor(textFilePath);
+                        //fileProcessor.Process();
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"KLAIDA: nepalaikomas tipas {fileType} ");
+                    return;
+            }
+        }
+
+    }
+    }
+
---
+++ WorkingWithFiles/WorkingWithFilesBL/FileProcessor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFilesBL
+{
+    public class FileProcessor
+    {
+        public string InputFilePath { get; }
+        private static readonly string _backupDirectoryName = "backup";
+        private static readonly string _inProgressDirectoryName = "processing";
+        private static readonly string _completeDirectoryName = "complete";
+        public FileProcessor(string filePath)
+        {
+           
+        InputFilePath = filePath;
+        }
+        public void Process()
+        {
+            Console.WriteLine($"Pradedu bylos apdorojima {InputFilePath}");
+            if(!File.Exists(InputFilePath))
+            {
+                Console.WriteLine($"KLAIDA: byla {InputFilePath} neegzistuoja.");
+                return;
+            }
+            DirectoryInfo directoryInfo = new DirectoryInfo(InputFilePath);
+            string rootDirectoryPath = directoryInfo.Parent.Parent.FullName;
+            Console.WriteLine($"Sakninis katalogas yra {rootDirectoryPath}");
+            string backupDirectoryPath = Path.Combine(rootDirectoryPath, _backupDirectoryName);
+            //if(!Directory.Exists(backupDirectoryPath))
+            //{
+            //    Console.WriteLine($"Kuriamas {backupDirectoryPath}");
+                  Directory.CreateDirectory(backupDirectoryPath);
+            //}
+            string inputFileName = Path.GetFileName(InputFilePath);
+            string backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+            Console.WriteLine($"Kopijuoju{ InputFilePath} i { backupFilePath}");
+            File.Copy(InputFilePath, backupFilePath);
+
+            Directory.CreateDirectory(Path.Combine(rootDirectoryPath, _inProgressDirectoryName));
+            string inProgressFilePath =
+                Path.Combine(rootDirectoryPath, _inProgressDirectoryName, inputFileName);
+            if(File.Exists(inProgressFilePath))
+            {
+                Console.WriteLine($"KLAIDA: byla {inProgressFilePath} jau apdorota.");
+                return;
+            }
+            Console.WriteLine($"Perkeliu {InputFilePath} i {inProgressFilePath}");
+            File.Move(InputFilePath, inProgressFilePath);
+            string extension = Path.GetExtension(InputFilePath);
+            switch (extension)
+            {
+                case ".txt":
+                    ProcessTextFile(inProgressFilePath);
+                    break;
+                default:
+                    Console.WriteLine($"Nepalaikomas bylos tipas {extension}");
+                    break;
+            }
+            string completeDirectoryPath = Path.Combine(rootDirectoryPath, _completeDirectoryName);
+            Directory.CreateDirectory(completeDirectoryPath);
+
+            Console.WriteLine($"Perkelimas {inProgressFilePath} i {completeDirectoryPath}");
+            var completeFileName = string.Format(
+                "{0}-{1}{2}",
+                Path.GetFileNameWithoutExtension(InputFilePath), Guid.NewGuid(), extension);
+            var completedFilePath = Path.Combine(completeDirectoryPath, completeFileName);
+            File.Move(inProgressFilePath, completedFilePath);
+
+            string inProgressDiectoryPath = Path.GetDirectoryName(inProgressFilePath);
+            Directory.Delete(inProgressFilePath, true);
+
+        }
+        private void ProcessTextFile (string inProgressFilePath)
+        {
+            Console.WriteLine($"Apdorojama byla {inProgressFilePath}");
+        }
+    }
+}
